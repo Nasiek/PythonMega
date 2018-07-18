fdf5487ae22b3b29ce566b4832edbcbab86aedd9
k <keisangittens@gmail.com>
2018-07-18 17:21:29 -0400

    looping

--- python Basics/open_fruits.py
+++ python Basics/open_fruits.py
@@ -1,4 +1,14 @@
-opener = open("fruits.txt")
-reader = opener.read()
-closer = opener.close()
-print(reader)
+myfile = open("fruits.txt")
+content = myfile.read()
+myfile.close()
+content = content.splitlines()
+for i in content:
+	print(len(i))
+
+
+file = open("fruits.txt")
+content = file.readlines()
+content=[line.strip() for line in content]
+file.close()
+for i in content:
+	print(len(i))
