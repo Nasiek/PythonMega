a72f9375a55eff8f8a491e307a58cae9e5cf3196
keisan <keisangittens@gmail.com>
2018-08-07 22:50:55 -0400

    added exception error message for word that doesn't exist in the dictionary.

--- App1/app.py
+++ App1/app.py
@@ -1,7 +1,19 @@
-def value(j):
-    j.load
-    i = input("Enter a word: ")
-    i
-    for word in i:
-        result = ['word']
-        print(result)
+import json
+
+data = json.load(open("data.json"))
+
+def translator(w):
+    if w in data:
+        return data[w]
+    else:
+        return "The word doesn't exist. Please recheck it. :("
+
+word = input("Enter a word: ")
+
+print(translator(word))
+    # jason.load
+    # i = input("Enter a word: ")
+    # i
+    # for word in i:
+    #     result = ['word']
+    #     print(result)
