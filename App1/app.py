f261b79e7dda57e7875d2d54e91931580aa86863
keisan <keisangittens@gmail.com>
2018-10-09 15:18:47 -0400

    added more functionality to program. using get_close_matches from difflib we can check if the wrong input is close to a value in our JSON file.

--- App1/app.py
+++ App1/app.py
@@ -1,16 +1,30 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
-def translator(w):
+def translate(w):
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w,data.keys())[0]) #or w,data.keys(),n=1)
+        if yn == "Y":
+            return data[get_close_matches(w,data.keys())[0]] 
+        elif yn == "N":
+            return "We don't have it."
+        else: 
+            return "Cannot process this query." 
     else:
         return "The word doesn't exist. Please recheck it. :("
 
 word = input("Enter a word: ")
 
-print(translator(word))
+output = translate(word)
+if  type(output) == list:
+    for item in output:
+        print(item)
+    else: 
+        print(output)
     # jason.load
     # i = input("Enter a word: ")
     # i
