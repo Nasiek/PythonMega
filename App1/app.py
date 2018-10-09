130d451886bde294c6e481c9249f43909cb489e0
keisan <keisangittens@gmail.com>
2018-10-09 15:34:03 -0400

    added string methods upper and capitalization case for parsing and accessing acronyms and proper nouns

--- App1/app.py
+++ App1/app.py
@@ -6,6 +6,10 @@ data = json.load(open("data.json"))
 def translate(w):
     if w in data:
         return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w,data.keys())[0]) #or w,data.keys(),n=1)
         if yn == "Y":
